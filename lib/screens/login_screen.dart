708b76d5a82f0796444788afb174a94844d16e68
douglaspereira <douglas.pereira@serpro.gov.br>
2019-10-31 12:14:40 -0300

    tela de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:gerente_loja/blocs/login_bloc.dart';
+import 'package:gerente_loja/widgets/input_field.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final _loginBloc = LoginBloc();
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      body: Stack(
+        alignment: Alignment.center,
         children: <Widget>[
-          Icon(Icons.business_center, color: Colors.pinkAccent, size: 160,)
+          Container(),
+          SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(
+                    Icons.store_mall_directory,
+                    color: Colors.pinkAccent,
+                    size: 160,
+                  ),
+                  InputField(
+                    icon: Icons.person_outline,
+                    hint: "Usuário",
+                    obscure: false,
+                    stream: _loginBloc.outEmail,
+                    onChanged: _loginBloc.changeEmail,
+                  ),
+                  InputField(
+                    icon: Icons.lock_outline,
+                    hint: "Senha",
+                    obscure: true,
+                    stream: _loginBloc.outPassword,
+                    onChanged: _loginBloc.changePassword,
+                  ),
+                  SizedBox(height: 32,),
+                  StreamBuilder<bool>(
+                    stream: _loginBloc.outSubmitValid,
+                    builder: (context, snapshot) {
+                      return SizedBox(
+                        height: 50,
+                        child: RaisedButton(
+                          color: Colors.pinkAccent,
+                          child: Text("Entrar"),
+                          onPressed: snapshot.hasData ? (){} : null,
+                          textColor: Colors.white,
+                          disabledColor: Colors.pinkAccent.withAlpha(140),
+                        ),
+                      );
+                    }
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
