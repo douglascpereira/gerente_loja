145ac440b5362833be75855c9daa3d79a3340a89
douglaspereira <douglas.pereira@serpro.gov.br>
2019-11-04 11:14:35 -0300

    tab de clientes com filtro

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:gerente_loja/blocs/user_bloc.dart';
 import 'package:gerente_loja/tabs/users_tab.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,12 +13,15 @@ class _HomeScreenState extends State<HomeScreen> {
   PageController _pageController;
   int _page = 0;
 
+  UserBloc _userBloc;
 
   @override
   void initState() {
     super.initState();
 
     _pageController = PageController();
+
+    _userBloc = UserBloc();
   }
 
 
@@ -61,18 +66,21 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: SafeArea(
-        child: PageView(
-          controller: _pageController,
-          onPageChanged: (p){
-            setState(() {
-              _page = p;
-            });
-          },
-          children: <Widget>[
-            UsersTab(),
-            Container(color: Colors.yellow,),
-            Container(color: Colors.green,)
-          ],
+        child: BlocProvider<UserBloc>(
+          bloc: _userBloc,
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (p){
+              setState(() {
+                _page = p;
+              });
+            },
+            children: <Widget>[
+              UsersTab(),
+              Container(color: Colors.yellow,),
+              Container(color: Colors.green,)
+            ],
+          ),
         ),
       ),
     );
