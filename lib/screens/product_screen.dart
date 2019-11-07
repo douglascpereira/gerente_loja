db8c952816685bddd202b22fe1962977dfef82dc
douglaspereira <douglas.pereira@serpro.gov.br>
2019-11-07 13:52:01 -0300

    tela de produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gerente_loja/blocs/product_bloc.dart';
 import 'package:gerente_loja/validators/product_validator.dart';
 import 'package:gerente_loja/widgets/images_widget.dart';
+import 'package:gerente_loja/widgets/product_sizes.dart';
 
 class ProductScreen extends StatefulWidget {
 
@@ -133,7 +134,27 @@ class _ProductScreenState extends State<ProductScreen> with ProductValidator {
                       keyboardType: TextInputType.numberWithOptions(decimal: true),
                       onSaved: _productBloc.savePrice,
                       validator: validatePrice,
-                    )
+                    ),
+                    SizedBox(height: 16,),
+                    Text(
+                      "Tamanhos",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 12
+                      ),
+                    ),
+                    ProductSizes(
+                      context: context,
+                      initialValue: snapshot.data["sizes"],
+                      onSaved: _productBloc.saveSizes,
+                      validator: (s){
+                        if(s.isEmpty) {
+                          return "";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
                   ],
                 );
               }
